1333df2c5e233ee93d740e6679bfbd9aa5119070
John Doe <john@sample.com>
2022-05-16 15:42:15 +0200

    my first project commit

---
+++ tests/calculator.js
@@ -0,0 +1,38 @@
+let homepage = require('../pages/homepage');
+
+describe('demo calculator tests', function () {
+
+    it('addition test', function () {
+
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo();
+
+        homepage.verifyResult('7');
+
+        browser.sleep(2000);
+
+
+    });
+
+    it('substraction test', function () {
+
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo1();
+
+        homepage.verifyResult('7');
+
+        browser.sleep(2000);
+
+
+    });
+});
